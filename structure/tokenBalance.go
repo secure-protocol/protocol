10eaa082f60d3d3aa718b9a57bd37cad7dacf45f
hugo <hugo.l@gmail.com>
2024-10-16 16:13:26 +0800

    tokenBalance 新增字段

--- structure/tokenBalance.go
+++ structure/tokenBalance.go
@@ -5,7 +5,7 @@ import "github.com/shopspring/decimal"
 type TokenBalance struct {
 	TokenID     uint            `json:"tokenID" gorm:"primarykey;comment:tokenID"`
 	Address     string          `json:"address" gorm:"primarykey;type:varchar(255)"`
-	AddressType string          `json:"addressType" gorm:"type:enum('receive','gather')'"`
+	AddressType string          `json:"addressType" gorm:"type:enum('receive','gather')"`
 	Balance     decimal.Decimal `json:"balance" gorm:"type:decimal(40,20);comment:余额"`
 	Occupy      decimal.Decimal `json:"occupy" gorm:"type:decimal(40,20);comment:占用"`
 }
